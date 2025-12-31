eaed31bf4b661ed53c39ed127d311f1a2f8fca24
ArfinHasib <arfinhasib21375@gmail.com>
2025-12-31 21:07:35 +0600

    Updates

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,24 @@ import express from 'express';
 
 const app = express();
 
+// Endpoint
 app.get('/api/notes', (req, res) => {
-	// Send the notes
-	res.send('you got 5 notes');
-	res.send('you got 7 notes');
+	res.status(200).send('You got 20 notes.');
+});
+
+// Endpoint
+app.post('/api/notes', (req, res) => {
+	res.status(201).send('Post created Successfully.');
+});
+
+// Endpoint
+app.put('/api/notes/:id', (req, res) => {
+	res.status(200).send('Note updated Successfully.');
+});
+
+// Endpoint
+app.delete('/api/notes/:id', (req, res) => {
+	res.status(200).send('Note deleted successfully.');
 });
 
 app.listen(5001, () => {
