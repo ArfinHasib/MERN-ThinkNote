96878dd67b35fbdc618efcc880ab62a781cfbe98
ArfinHasib <arfinhasib21375@gmail.com>
2026-01-11 11:57:27 +0600

    Mongo DB Setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
 
 const app = express();
+
+connectDB();
 
 app.use('/api/notes', notesRoutes);
 
 app.listen(5001, () => {
 	console.log('Server Started on PORT: 5001');
+	process.exit(1); // exit with failure
 });
