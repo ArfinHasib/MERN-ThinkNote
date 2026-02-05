6137a450050badc491d00ecadd9762a2975312af
ArfinHasib <arfinhasib21375@gmail.com>
2026-02-05 21:23:39 +0600

    Tailwind CSS Installation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,17 @@ import { Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';
 import NoteDetailPage from './pages/NoteDetailPage';
+import toast from 'react-hot-toast';
 
 const App = () => {
 	return (
 		<div>
+			<button
+				onClick={() => toast.error('This is not Awesome')}
+				className="p-4 bg-red-500 text-white rounded-md left-0 top-3 my-2"
+			>
+				Click to Toast
+			</button>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/create" element={<CreatePage />} />
