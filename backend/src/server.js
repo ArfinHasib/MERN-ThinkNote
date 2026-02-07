bf1a867d804e52a25b4dfd8c62a2b9ca33c94b7d
ArfinHasib <arfinhasib21375@gmail.com>
2026-02-07 16:59:17 +0600

    Enable CORS and Fixing The CORS Error

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import notesRoutes from './routes/notesRoutes.js';
-import { connectDB } from './config/db.js';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
+import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 dotenv.config();
 
@@ -14,6 +15,11 @@ const PORT = process.env.PORT || 5001;
 // Middleware
 app.use(express.json()); // This middleware will parese JSON bodies: req.body
 app.use(rateLimiter);
+app.use(
+	cors({
+		origin: 'http://localhost:5173', // Allow requests from this origin
+	}),
+);
 // Our simple custom middleware
 // app.use((req, res, next) => {
 // 	console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
