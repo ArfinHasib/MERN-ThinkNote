30baa937be7d9a5439b850efbb2ba7f975d15f98
ArfinHasib <arfinhasib21375@gmail.com>
2026-02-07 14:14:34 +0600

    Daisy UI Init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,6 @@ import toast from 'react-hot-toast';
 const App = () => {
 	return (
 		<div>
-			<button
-				onClick={() => toast.error('This is not Awesome')}
-				className="p-4 bg-red-500 text-white rounded-md left-0 top-3 my-2"
-			>
-				Click to Toast
-			</button>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/create" element={<CreatePage />} />
